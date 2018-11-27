195cd31d10e14670afc1025f8a60987d333bf92d
Paul Hine <paul@hine.works>
2018-11-27 11:22:41 +0100

    simplified

--- lib.js
+++
@@ -1,9 +0,0 @@
-export const loadImage = (sk, path) =>
-  new Promise((resolve, reject) => {
-    sk.loadImage(path, resolve, reject)
-  })
-
-export const overlayMap = (map, canvas) =>
-  new Promise(resolve => {
-    map.overlay(canvas, resolve)
-  })
--- main.js
+++ main.js
@@ -1,88 +1,104 @@
-import Mappa from 'mappa-mundi'
-require('@code-dot-org/p5')
-require('@code-dot-org/p5.play/lib/p5.play')
-import { loadImage, overlayMap } from './lib'
-import mapConfig from './map.config'
+const mapConfig = {
+  mapboxKey:
+    'pk.eyJ1IjoicHpoaW5lIiwiYSI6ImNqb3lpc3d5NjJieDMzcGtmemVscWQ0ZnkifQ.FzHR5HQu1lAHCcqI9g4HFg',
+  mappaOptions: {
+    studio: true,
+    username: 'pzhine',
+    style: 'cjoyiufdc3zhi2smr3ws3upbr',
+    lng: 2.171309,
+    lat: 41.403057,
+    zoom: 13.5,
+    pitch: 0,
+    logo: false,
+  },
+}
 
-console.log('play', play)
+const loadImageP = path =>
+  new Promise((resolve, reject) => {
+    loadImage(path, resolve, reject)
+  })
+
+const overlayMapP = (map, canvas) =>
+  new Promise(resolve => {
+    map.overlay(canvas, resolve)
+  })
 
 const mappa = new Mappa('Mapbox', mapConfig.mapboxKey)
-const TILE_W = 1280
-const TILE_H = 1280
-const TILE_ROWS = 3
-const TILE_COLS = 3
+const TILE_W = 50
+const TILE_H = 50
 
 let table
-let imgGrid
 let canvas
-let mapImg
+let tileMap
+let drops
+let speed = 1
 
-const generateMapGrid = (sk, latLngGrid) => {
-  // generate a 2d array of images that form a seamless map when rendered
-  latLngGrid.forEach((gridRow, row) => {
-    gridRow.forEach((latLng, col) => {
-      const currMap = mappa.staticMap({
-        ...mapConfig.mappaOptions,
-        lat: latLng.lat,
-        lng: latLng.lng,
-        width: TILE_W,
-        height: TILE_H,
+const drawDrops = () => {
+  drops = []
+
+  const drawRow = i => {
+    //get the lat/lng of each house
+    try {
+      const pos = tileMap.map.latLngToContainerPoint(
+        L.latLng(
+          table.getString(i, 'LATITUD_Y'),
+          table.getString(i, 'LONGITUD_X'),
+        ),
+      )
+      drops.push({
+        pos,
+        size: 10,
       })
-      loadImage(sk, currMap.imgUrl).then(img => {
-        sk.image(img, col * TILE_W, row * TILE_H)
-      })
-    })
+    } catch (err) {
+      console.error(err)
+    }
+    setTimeout(() => drawRow(i + 1), 100 / speed)
+  }
+
+  drawRow(0)
+}
+
+function preload() {
+  table = loadTable(
+    'http://www.bcn.cat/huts/hut_comunicacio_opendata.csv',
+    'csv',
+    'header',
+  )
+}
+
+function setup() {
+  canvas = createCanvas(window.innerWidth, window.innerHeight).parent('root')
+
+  // console.log('rows', table.getRowCount())
+
+  tileMap = mappa.tileMap({
+    ...mapConfig.mappaOptions,
+    style: `mapbox://styles/${mapConfig.mappaOptions.username}/${
+      mapConfig.mappaOptions.style
+    }`,
+    width: window.innerWidth,
+    height: window.innerHeight,
   })
+
+  overlayMapP(tileMap, canvas)
+
+  tileMap.onChange(drawDrops)
 }
 
-const sketch = sk => {
-  sk.preload = () => {
-    // table = sk.loadTable(
-    //   'http://www.bcn.cat/huts/hut_comunicacio_opendata.csv',
-    //   'csv',
-    //   'header',
-    // )
+function draw() {
+  clear()
+  if (!drops) {
+    return
   }
-  sk.setup = () => {
-    canvas = sk
-      .createCanvas(window.innerWidth, window.innerHeight)
-      .parent('root')
-    // console.log('rows', table.getRowCount())
-
-    // load overview map as overlay to calc tile lat/lng centers
-    const tileMap = mappa.tileMap({
-      ...mapConfig.mappaOptions,
-      style: `mapbox://styles/${mapConfig.mappaOptions.username}/${
-        mapConfig.mappaOptions.style
-      }`,
-      width: window.innerWidth,
-      height: window.innerHeight,
-    })
-    const tileW = Math.round(window.innerWidth / TILE_COLS)
-    const tileH = Math.round(window.innerHeight / TILE_ROWS)
-    console.log('tileW/tileH', tileW, tileH)
-
-    overlayMap(tileMap, canvas).then(() => {
-      // create grid of lat/lng centers
-      const grid = []
-      for (let row = 0; row < TILE_ROWS; row++) {
-        grid[row] = []
-        for (let col = 0; col < TILE_COLS; col++) {
-          const x = tileW * col + Math.round(tileW / 2)
-          const y = tileH * row + Math.round(tileH / 2)
-          sk.ellipse(x, y, 10)
-          const latlng = tileMap.map.containerPointToLatLng(L.point(x, y))
-          grid[row][col] = latlng
-        }
-      }
-
-      generateMapGrid(sk, grid)
-    })
-  }
-  sk.draw = () => {
-    console.log('camera', createSprite)
-    camera.zoom = 1
+  for (drop of drops) {
+    noStroke()
+    fill(255, 255, 255)
+    drop.size += 0.05 * speed
+    ellipse(drop.pos.x, drop.pos.y, drop.size)
   }
 }
 
-export default p5(sketch)
+function mouseClicked() {
+  speed += 20
+  console.log('speed', speed)
+}
